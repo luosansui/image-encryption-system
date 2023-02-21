c2575ee998300f187f1dc5a76c64b7ec29a2654a
luosansui <3099688664@qq.com>
2023-02-21 22:45:17 +0800

    bugfix: 修复文件重复选择问题及删除图片时重新计算图片url问题

--- src/components/Upload/index.tsx
+++ src/components/Upload/index.tsx
@@ -1,16 +1,16 @@
-import { calculateMD5 } from "@/utils/module/file";
-import React, { useRef, useState } from "react";
+import { calculateMD5 } from "@/utils/file";
+import React, { useEffect, useRef, useState } from "react";
 import Dropzone from "react-dropzone";
 import pLimit from "p-limit";
+import { produce } from "immer";
+import { FileType } from "@/type/file";
 
-export type FileWithMD5 = {
-  file: File;
-  md5: string;
-};
-
-const MultiImageUpload: React.FC = () => {
-  const [files, setFiles] = useState<File[]>([]);
-  const fileHashSet = useRef<Set<string>>(new Set());
+const Upload: React.FC<{
+  onChange?: (files: FileType[]) => void;
+  className?: string;
+}> = ({ onChange, className }) => {
+  const [files, setFiles] = useState<FileType[]>([]);
+  const tempFileHashSet = useRef(new Set<string>());
   /**
    * 过滤重复文件
    * @param files 文件列表
@@ -18,13 +18,18 @@ const MultiImageUpload: React.FC = () => {
    */
   async function filterDuplicateFiles(files: File[]) {
     const limit = pLimit(3);
-    const uniqueFiles: File[] = [];
+    const uniqueFiles: FileType[] = [];
     await Promise.all(
       files.map(async (file) => {
-        const md5 = await limit(() => calculateMD5(file));
-        if (!fileHashSet.current.has(md5)) {
-          fileHashSet.current.add(md5);
-          uniqueFiles.push(file);
+        try {
+          const md5 = await limit(() => calculateMD5(file));
+          if (!tempFileHashSet.current.has(md5)) {
+            tempFileHashSet.current.add(md5);
+            const src = URL.createObjectURL(file);
+            uniqueFiles.push({ file, md5, src });
+          }
+        } catch (error) {
+          console.error(error);
         }
       })
     );
@@ -33,59 +38,93 @@ const MultiImageUpload: React.FC = () => {
 
   const handleDrop = async (acceptedFiles: File[]) => {
     const newFiles = await filterDuplicateFiles(acceptedFiles);
-    setFiles((prevState) => [...prevState, ...newFiles]);
+    setFiles(
+      produce((draftState) => {
+        draftState.push(...newFiles);
+      })
+    );
   };
 
-  const handleRemove = (file: File) => {
-    setFiles((prevState) => prevState.filter((f) => f !== file));
+  const handleRemove = (md5: string) => {
+    setFiles((prevState) => prevState.filter((file) => file.md5 !== md5));
+    tempFileHashSet.current.delete(md5);
   };
 
+  /**
+   * 文件列表变更时通知上级组件
+   */
+  useEffect(() => {
+    onChange?.(files);
+  }, [files]);
   return (
-    <div className="flex flex-wrap">
+    <div className={`flex flex-wrap ${className ?? ""}`}>
       <Dropzone onDrop={handleDrop}>
         {({ getRootProps, getInputProps }) => (
-          <div className="p-2 box-border w-1/3 md:w-1/4 lg:w-1/6 2xl:w-1/12">
+          <div className="sticky top-0 p-2 box-border w-1/3 md:w-1/4 lg:w-1/6 2xl:w-1/12">
             <div
-              className="h-32 p-2 border-2 border-dashed border-gray-400 rounded cursor-pointer select-none"
+              className="h-32 p-2 border-2 border-dashed border-gray-400 rounded cursor-pointer select-none flex justify-center items-center"
               {...getRootProps()}
             >
               <input {...getInputProps()} />
-              <p>click to select files</p>
+              <svg
+                className="h-10 w-10 text-gray-600"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M12 4v16m8-8H4"
+                />
+              </svg>
             </div>
           </div>
         )}
       </Dropzone>
-      {files.map((file, index) => (
+      {files.map((FileType) => (
         <div
-          key={file.name}
+          key={FileType.md5}
           className="p-2 box-border relative w-1/3 md:w-1/4 lg:w-1/6 2xl:w-1/12"
         >
           <img
-            src={URL.createObjectURL(file)}
-            alt={file.name}
-            className="w-full h-32 object-cover rounded"
+            src={FileType.src}
+            alt={FileType.file.name}
+            className="w-full h-32 object-cover rounded border border-gray-200"
           />
           <button
-            className="text-black absolute -top-1 -right-1"
-            onClick={() => handleRemove(file)}
+            className="text-black absolute top-0 right-0"
+            onClick={() => handleRemove(FileType.md5)}
           >
             <svg
+              viewBox="0 0 1024 1024"
+              version="1.1"
               xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              className="w-6 h-6"
+              p-id="902"
+              width="24"
+              height="24"
             >
               <path
-                fillRule="evenodd"
-                d="M13.414 6.586a2 2 0 112.828 2.828L12.828 10l3.414 3.414a2 2 0 11-2.828 2.828L10 12.828l-3.414 3.414a2 2 0 11-2.828-2.828L7.172 10 3.758 6.586a2 2 0 112.828-2.828L10 7.172l3.414-3.414z"
-                clipRule="evenodd"
-              />
+                d="M512 512m-460.8 0a460.8 460.8 0 1 0 921.6 0 460.8 460.8 0 1 0-921.6 0Z"
+                fill="#E73A30"
+                p-id="903"
+              ></path>
+              <path
+                d="M584.3968 512l108.6464 108.5952a51.2 51.2 0 0 1-72.448 72.448L512 584.3968l-108.5952 108.6464a51.2 51.2 0 1 1-72.448-72.448L439.6032 512 330.9568 403.4048A51.2 51.2 0 0 1 403.456 330.9568L512 439.6032l108.5952-108.6464a51.2 51.2 0 0 1 72.448 72.448L584.3968 512z"
+                fill="#FFFFFF"
+                p-id="904"
+              ></path>
             </svg>
           </button>
         </div>
       ))}
+      {/* <div className="absolute -bottom-2 left-2 text-xs font-semibold text-gray-600">
+        {files.length ? `共${files.length}张图片` : null}
+      </div> */}
     </div>
   );
 };
 
-export default MultiImageUpload;
+export default Upload;
--- src/pages/encryption/index.tsx
+++ src/pages/encryption/index.tsx
@@ -1,13 +1,37 @@
 import ControlPanel from "@/components/ControlPanel";
 import Upload from "@/components/Upload";
+import { useState } from "react";
+import { FileType } from "@/type/file";
 
 export default function Encryption() {
+  const [fileList, setFileList] = useState<FileType[]>([]);
+
+  /**
+   * 处理上传列表变更
+   */
+  const handleFileListChange = (files: FileType[]) => {
+    console.log("files", files);
+    setFileList(files);
+  };
   return (
     <div className="flex h-full">
-      <div className="flex-1 h-fit p-2 border-2 border-gray-200 border-dashed rounded-lg">
-        <Upload></Upload>
+      <div className="flex-1 flex flex-col">
+        <div className="flex-1 p-2 border-2 border-gray-200 rounded-lg overflow-y-auto overflow-x-hidden">
+          <Upload className="" onChange={handleFileListChange}></Upload>
+        </div>
+
+        <div className="flex-1 p-2 mt-3 mb-3 border-2 border-gray-200 rounded-lg overflow-y-auto overflow-x-hidden">
+          <Upload className="" onChange={handleFileListChange}></Upload>
+        </div>
+        <div className="h-fit p-2 border-2 border-gray-200 shadow-sm rounded-lg">
+          进度条
+        </div>
       </div>
-      <div className="w-1/6 h-full min-w-fit ml-4 p-2 border-2 border-gray-200 border-dashed rounded-lg">
+      {/* <div className="flex-1 h-fit  "> */}
+      {/* 上传区 */}
+      {/* 
+      </div> */}
+      <div className="w-1/6 h-full min-w-fit ml-4 p-2 border-2 border-gray-200 rounded-lg">
         {/* 控制区 */}
         <ControlPanel />
       </div>
