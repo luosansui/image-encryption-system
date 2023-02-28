6df4b35520f988d6998b287398840549e31b698e
umaru <3099688664@qq.com>
2023-02-28 12:38:36 +0800

    bugfix: 修改较优线程数计算方式

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,9 @@
 import { twoThirds } from "./number";
 export const getThreadsNumber = (num: number) => {
   if (num >= 1) {
-    return Math.max(
-      Math.min(Math.floor(num / 3), twoThirds(navigator.hardwareConcurrency)),
-      1
+    return Math.min(
+      Math.ceil(num / 3),
+      twoThirds(navigator.hardwareConcurrency)
     );
   }
   return 1;
