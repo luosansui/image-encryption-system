d1ee33aa369a4d32b062aa073e37760430cb8d93
hoshyo <58138234+hoshyo@users.noreply.github.com>
2023-03-04 21:22:12 +0800

    bugfix: 修复图片裁剪弹框缺陷

---
+++ src/components/ImageCrop/constant.ts
@@ -0,0 +1,12 @@
+export const DEFAULT_SCALE = {
+  min: 0,
+  max: 100,
+  defaultValue: 0,
+  suffix: "%",
+};
+export const DEFAULT_ROTATE = {
+  min: -180,
+  max: 180,
+  defaultValue: 0,
+  suffix: "°",
+};
---
+++ src/utils/object.ts
@@ -0,0 +1,36 @@
+/**
+ *
+ * @param objA 对象
+ * @param objB 对象
+ * @returns 两对象的浅比较结果
+ */
+export const shallowEqual = (objA: any, objB: any) => {
+  if (objA === objB) {
+    return true;
+  }
+
+  if (
+    typeof objA !== "object" ||
+    objA === null ||
+    typeof objB !== "object" ||
+    objB === null
+  ) {
+    return false;
+  }
+
+  const keysA = Object.keys(objA);
+  const keysB = Object.keys(objB);
+
+  if (keysA.length !== keysB.length) {
+    return false;
+  }
+
+  for (let i = 0; i < keysA.length; i++) {
+    const key = keysA[i];
+    if (objA[key] !== objB[key]) {
+      return false;
+    }
+  }
+
+  return true;
+};
